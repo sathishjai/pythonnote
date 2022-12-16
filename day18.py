1e448a6526ebd5e13eae5b7baad48fa80e5760e5
sathishjai <sathishjai2001@gmail.com>
2022-12-16 15:47:25 +0530

    day18 class note

---
+++ day18.py
@@ -0,0 +1,74 @@
+  paradiym 
+  objecte oriented programming
+  procedure 
+  functional
+  modular programing
+
+#object oriented programming
+  object
+  class
+  inheritance
+  polymorphosm
+  abstraction
+  encapsulation
+  interface
+
+class 
+    class is a template 
+    class is a blueprint
+
+object
+    repressentatives of bank
+    manager=bank
+    state,behariour
+    object-combination of stats and behaviars
+    object-interest ,accopen ,address changes
+    object-represents class
+    object is memory reference of a class
+
+
+templates
+    bank
+    customer care
+    office
+    airport
+    bus stand
+    suber markcet
+
+(in python ,everything is object
+class is a template /blue print)
+
+multiparadiym language
+    poradigm(idea)
+
+
+inheritance:
+    an object of one class behaving as an object of another class 
+    humanbeing -template-class
+    object-states and behaviour
+        states 
+        behaviour
+
+reusability
+    another person writing code modules name use another person that is a code reuseability
+
+    reuseability saporting methods
+        inheritance
+        polymorphism
+        abstraction
+
+encapsulation (data binding )
+    human being -personal -atm-lock -diary-->personal
+    personal -friends -->module
+    relational 
+    public
+
+patent child -->loosly coupled relationship
+
+abstraction(data hiding)(synopsis)(functions)-->related
+    ex:
+        car :class -->steering ,gear,acce,cluth,brake
+    showing only the necessary data and hiding the unwanted one 
+
+polymorphism    (poly-->many)
+    showing different faces at different places
